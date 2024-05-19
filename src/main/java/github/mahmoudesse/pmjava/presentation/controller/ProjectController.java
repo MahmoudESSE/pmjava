9246bb78f4b4de9c07370dcaff5b3609d144cc3e
MahmoudESSE <mahmoudessehayli@gmail.com>
2024-05-19 21:16:22 +0100

    chore: move file from dist to static
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- src/main/java/github/mahmoudesse/pmjava/presentation/controller/ProjectController.java
+++ src/main/java/github/mahmoudesse/pmjava/presentation/controller/ProjectController.java
@@ -10,12 +10,19 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @AllArgsConstructor
+@RequestMapping("/project")
 @Controller
 public class ProjectController {
   private final IProjectService ps;
+
+  @GetMapping("/")
+  public String indexProject() {
+    return "projects/index";
+  }
 
   @GetMapping("/listProjects")
   public String listProjects(Model model) {
